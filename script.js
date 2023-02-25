01f0aa387338e338663298788b72006a09b37a01
dirtydenny <dallen7@socal.rr.com>
2023-02-24 22:34:29 -0800

    after debug w tutor

--- script.js
+++ script.js
@@ -3,218 +3,212 @@
 // setup arrays with acceptable characters
 
 var specialCharacters = [
-  ' ',
-  '!',
+  " ",
+  "!",
   '"',
-  '#',
-  '$',
-  '%',
-  '&'
-  "'",
-  '(',
-  ')',
-  '*',
-  '+',
-  ',',
-  '-',
-  '.',
-  '/',
-  ':',
-  ';',
-  '<',
-  '=',
-  '>',
-  '?',
-  '@',
-  '[',
+  "#",
+  "$",
+  "%",
+  "&",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
   "/",
-  ']',
-  '^',
-  '_',
-  '`',
-  '{',
-  '|',
-  ',',
-  '}',
-  '~'
- ];
-
- var lwrCaseLetters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
- ];
-
- var = uprCaseLetters [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
- ];
-
- var = zerotonine[
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9'
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "/",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  ",",
+  "}",
+  "~",
 ];
 
+var lwrCaseLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
+var uprCaseLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var zerotonine = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function getPwdLength() {
+  var length = parseInt(
+    prompt("How many characters do you need in your password: 8-128?")
+  );
+
+  if (length >= 8 && length <= 128) {
+    // other critriea
+  } else {
+    alert("Password lenght must be 8 - 128 characters");
+    getPwdLength();
+  }
+}
 
 function getPasswordOptions() {
-  var length = parseInt(
-    prompt("How many characters do you need in your password: 8-128?"),
-    10
-  );
+  // get the pwd len
+  getPwdLength();
 
-
-  if (length < 8 || > 128) {
-    alert('Password lenght must be 8 - 128 characters');
-    return null;
-  }
-
-  var hasNumbers = window.confirm('Click Ok to include numbers.'
+  var hasNumbers = window.confirm("Click Ok to include numbers.");
+  var haslwrCaseLetters = window.alert(
+    "Click ok to include lower case letters."
   );
-  var haslwrCaseLetters = window.alert('Click ok to include lower case letters.'
+  var hasuprCaseLetters = window.alert("Click to include UPPER CASE letters.");
+  var hasspecialCharacters = window.alert(
+    "Click to include Special Characters."
   );
-  var hasuprCaseLetters = window.alert('Click to include UPPER CASE letters.'
-  ):
-  var hasspecialCharacters = window.alert('Click to include Special Characters.'
-  ):
 
   if (
-    hasNumbers === false &&
+    hasZerotonine === false &&
     haslwrCaseLetters === false &&
     hasuprCaseLetters === false &&
     hasspecialCharacters === false
-  ) {
-    alert ('You must pick at least one character type');
-    return null;
-// you must have at least one variable, see quinton's lecture yesterday's
+  ) 
+     else {
+    alert("You have to pick something.");
+    getPwdLength();
   }
-    
-// Object to store data
+  /*  alert("You must pick at least one character type");
+    return null; */
+    // you must have at least one variable, see quinton's lecture yesterday's
+  }
 
-var pswdOptions = {
-  Length: length,
-  hasNumbers: hasNumbers,
-  haslwrCaseLetters: haslwrCaseLetters,
-  hasuprCaseLetters: hasuprCaseLetters,
-  hasspecialCharacters: hasspecialCharacters,
-};
-return PasswordOptions;
+  // Object to store data
+
+  var pswdOptions = {
+    Length: length,
+    hasZerotonine: hasZerotonine,
+    haslwrCaseLetters: haslwrCaseLetters,
+    hasuprCaseLetters: hasuprCaseLetters,
+    hasspecialCharacters: hasspecialCharacters,
+  };
+  return PasswordOptions;
+}
+
+function getRandom(array) {
+  var randIndex = math.floor(Math.random() * array.length);
+  var randElement = arr[randIndex];
+  
+  return randElement;
+
 }
 
 
-/* from: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript?page=2&tab=scoredesc#tab-top
-
-
-function randomString() {
- var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
- var string_length = 8;
- var randomstring = '';
- for (var i=0; i<string_length; i++) {
-  var rnum = Math.floor(Math.random() * chars.length);
-  randomstring += chars.substring(rnum,rnum+1);
- }
- document.randform.randomfield.value = randomstring;
-}
-*/ 
-function randomString() {
- var chars = if options = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
- var string_length = 8;
- var randomstring = '';
- for (var i=0; i<string_length; i++) {
-  var rnum = Math.floor(Math.random() * chars.length);
-  randomstring += chars.substring(rnum,rnum+1);
- }
- document.randform.randomfield.value = randomstring;
-}
-*/
-function  generatePassword() {
+function generatePassword() {
+  
+  console.log("generate password function");
+  
   var options = getPasswordOptions();
 
   var result = [];
 
+  var typesofCharacters = []; 
 
-  
+  var musthaveCharacters = [];
+
+  if (options.hasZerotonine) {
+    typesofCharacters = typesofCharacters.concat(zerotonine);
+    musthaveCharacters
+  }
+
+
+
+for (var i = 0; i < options.length; i++) {
+  var typesofCharacters =getRandom(typesofCharacters);
+  result.push(typesofCharacters)
 }
 
 
-/*for (var i = 0; i < zooAnimals.length; i++) { 
+}
+
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+  console.log("Inside writePassword function");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+  /*for (var i = 0; i < zooAnimals.length; i++) { 
     console.log("I am going to zoo to see " + zooAnimals[i] + ".");
 }
 */
-for (var i = 0; i < )
+  // for (var i = 0; i < )
 
+  // Write password to the #password input
+}
 
-
+generateBtn.addEventListener("click", writePassword);
 
 // take user's criteria, select random characters, concatenate selecetion, and scramble.
 // print results to the screen/console
-
-
-
-
-  }
-
-  )
-
-}
-
-
-
 
 /* used
 alert("You must choose at least one variable.");
@@ -223,24 +217,8 @@ alert("Do you want UPPER CASE letters?");
 alert("Do you want numbers?");
 */
 
-
-
-
 // you must have at least one variable, see quinton's lecture yesterday's
 
-
-
-
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 /*
 flow questions:
 Do you want numbers?
@@ -276,3 +254,17 @@ generateBtn.addEventListener("click", writePassword);
     }
   }
   */
+/* from: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript?page=2&tab=scoredesc#tab-top
+
+
+function randomString() {
+ var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+ var string_length = 8;
+ var randomstring = '';
+ for (var i=0; i<string_length; i++) {
+  var rnum = Math.floor(Math.random() * chars.length);
+  randomstring += chars.substring(rnum,rnum+1);
+ }
+ document.randform.randomfield.value = randomstring;
+}
+*/
